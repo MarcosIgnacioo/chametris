9998d41c65f94f30e993e6b235ae36a0817d2e12
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-09-16 08:34:41 -0700

    build

--- src/todo.ts
+++ src/todo.ts
@@ -1,68 +1,68 @@
-// this.tetrisShapes.forEach(shape => {
-//   if (shape.y + 10 < 500) {
-//     shape.y += (Math.random() * 10) * GRAVITY;
-//   }
-// });
-// 
-// if (this.currentShape.worldRow + this.currentShape.height < this.canvas.totalRows) {
-//   this.currentShape.worldRow++;
-// } else {
-//   const shape = this.currentShape
-//   const currRow = this.currentShape.worldRow;
-//   const currCol = this.currentShape.worldCol;
-//   for (let row = currRow, mRow = 0; row < currRow + shape.shapeMatrix.length; row++) {
-//     for (let col = currCol - shape.shapeMatrix[0].length, mCol = 0; col < currCol; col++) {
-//       if (shape.shapeMatrix[mRow][mCol]) {
-//         canvas.drawRect(col * (SQUARE_SIZE), row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, "blue")
-//       }
-//       mCol++;
+// // this.tetrisShapes.forEach(shape => {
+// //   if (shape.y + 10 < 500) {
+// //     shape.y += (Math.random() * 10) * GRAVITY;
+// //   }
+// // });
+// // 
+// // if (this.currentShape.worldRow + this.currentShape.height < this.canvas.totalRows) {
+// //   this.currentShape.worldRow++;
+// // } else {
+// //   const shape = this.currentShape
+// //   const currRow = this.currentShape.worldRow;
+// //   const currCol = this.currentShape.worldCol;
+// //   for (let row = currRow, mRow = 0; row < currRow + shape.shapeMatrix.length; row++) {
+// //     for (let col = currCol - shape.shapeMatrix[0].length, mCol = 0; col < currCol; col++) {
+// //       if (shape.shapeMatrix[mRow][mCol]) {
+// //         canvas.drawRect(col * (SQUARE_SIZE), row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, "blue")
+// //       }
+// //       mCol++;
+// //     }
+// //     mRow++;
+//
+// import { TetrisShape } from "./TetrisShape";
+//
+// //   }
+// for (let mRow = 0; mRow < this.shapeMatrix.length; mRow++) {
+//   for (let mCol = 0; mCol < this.shapeMatrix[0].length; mCol++) {
+//     matrix += (worldMap[worldRow][worldCol])
+//     if (
+//       this.shapeMatrix[mRow][mCol] == 1 &&
+//       worldMap[worldRow][worldCol] == 0) {
+//       totalPixels++;
 //     }
-//     mRow++;
-
-import { TetrisShape } from "./TetrisShape";
-
+//     if (
+//       this.shapeMatrix[mRow][mCol] == 1 &&
+//       worldMap[worldRow][worldCol] == 1) {
+//       mRow = Infinity;
+//       break;
+//     }
+//     worldCol++;
 //   }
-for (let mRow = 0; mRow < this.shapeMatrix.length; mRow++) {
-  for (let mCol = 0; mCol < this.shapeMatrix[0].length; mCol++) {
-    matrix += (worldMap[worldRow][worldCol])
-    if (
-      this.shapeMatrix[mRow][mCol] == 1 &&
-      worldMap[worldRow][worldCol] == 0) {
-      totalPixels++;
-    }
-    if (
-      this.shapeMatrix[mRow][mCol] == 1 &&
-      worldMap[worldRow][worldCol] == 1) {
-      mRow = Infinity;
-      break;
-    }
-    worldCol++;
-  }
-  if (totalPixels == this.pixels) {
-    return worldRow;
-  }
-  worldRow++;
-  matrix += "\n"
-}
-
-shadow() {
-  for (let i = 0; i < this.canvas.map.length; i++) {
-    for (let j = 0; j < this.canvas.map[0].length; j++) {
-      if (this.canvas.map[i][j] == 2) {
-        this.canvas.map[i][j] = 0
-      }
-    }
-  }
-  let shadow = { ... this.currentShape }
-  shadow.worldRow = this.getHighestRowPossible(shadow.worldCol, true)
-  this.canvas.placeInWorld(shadow as TetrisShape, 2);
-}
-
-for (let row = 0; row < rowsToRemove.length; row++) {
-  const rowToRemove = rowsToRemove[row]
-  for (let col = 0; col < worldMap[0].length; col++) {
-    worldMap[rowToRemove][col] = 0;
-    worldMap[rowToRemove][col] = worldMap[rowToRemove - 1][col];
-    worldMap[rowToRemove - 1][col] = 0;
-  }
-}
+//   if (totalPixels == this.pixels) {
+//     return worldRow;
+//   }
+//   worldRow++;
+//   matrix += "\n"
+// }
+//
+// shadow() {
+//   for (let i = 0; i < this.canvas.map.length; i++) {
+//     for (let j = 0; j < this.canvas.map[0].length; j++) {
+//       if (this.canvas.map[i][j] == 2) {
+//         this.canvas.map[i][j] = 0
+//       }
+//     }
+//   }
+//   let shadow = { ... this.currentShape }
+//   shadow.worldRow = this.getHighestRowPossible(shadow.worldCol, true)
+//   this.canvas.placeInWorld(shadow as TetrisShape, 2);
+// }
+//
+// for (let row = 0; row < rowsToRemove.length; row++) {
+//   const rowToRemove = rowsToRemove[row]
+//   for (let col = 0; col < worldMap[0].length; col++) {
+//     worldMap[rowToRemove][col] = 0;
+//     worldMap[rowToRemove][col] = worldMap[rowToRemove - 1][col];
+//     worldMap[rowToRemove - 1][col] = 0;
+//   }
+// }
